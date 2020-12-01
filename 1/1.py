1ef1c4c8cc1f555ed7d6d3eb7fd4986d7d670c51
Kien Tuong Truong <kientuong@gmail.com>
2020-12-01 18:00:14 +0100

    refactor: minor refactor of day1 + oneliner solution

--- 1/1.py
+++ 1/1.py
@@ -1,13 +1,19 @@
+import timeit
+
+def old_solution(nums):
+    d = set(nums)
+    for l in inp:
+        if 2020 - l in d:
+            return l * (2020 - l)
+
+def one_liner(nums):
+    return next(l*(2020-l) for l in nums if (2020-l) in set(nums))
+
 if __name__ == "__main__":
     with open('1.in') as f:
         inp = f.readlines()
     inp = [int(l.strip()) for l in inp]
-    d = set()
-    for l in inp:
-        if 2020 - l in d:
-            n1 = l
-            n2 = 2020 - l
-            print(n1 * n2)
-            break
-        else:
-            d.add(l)
+    print(f"old solution: {old_solution(inp)}")
+    print("time for 10k runs:", timeit.timeit('old_solution(inp)', setup="from __main__ import old_solution, inp", number=10000))
+    print(f"one liner: {one_liner(inp)}")
+    print("time for 10k runs:", timeit.timeit('one_liner(inp)', setup="from __main__ import one_liner, inp", number=10000))
--- 1/2.py
+++ 1/2.py
@@ -1,16 +1,20 @@
+import timeit 
+
+def old_solution(nums):
+    d = set(nums)
+    for s, l in enumerate(inp):
+        for l1 in inp[s:]:
+            if 2020 - l - l1 in d:
+                return l * l1 * (2020 - l - l1)
+
+def one_liner(nums):
+    return next(l*l1*(2020-l-l1) for s, l in enumerate(nums) for l1 in nums[s:] if (2020-l-l1) in set(nums))
+
 if __name__ == "__main__":
     with open('1.in') as f:
         inp = f.readlines()
     inp = [int(l.strip()) for l in inp]
-    d = set()
-    for s, l in enumerate(inp):
-        for l1 in inp[s:]:
-            if 2020 - l - l1 in d:
-                n1 = l
-                n2 = l1
-                n3 = 2020 - l - l1
-                print(n1 * n2 * n3)
-                break
-            else:
-                d.add(l)
-
+    print(f"old solution: {old_solution(inp)}")
+    print("time for 10k runs:", timeit.timeit('old_solution(inp)', setup="from __main__ import old_solution, inp", number=10000))
+    print(f"one liner: {one_liner(inp)}")
+    print("time for 10k runs:", timeit.timeit('one_liner(inp)', setup="from __main__ import one_liner, inp", number=10000))
