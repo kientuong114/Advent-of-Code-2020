ccc36d81492fc1e0508326c9167b5407806bf767
Kien Tuong Truong <kientuong@gmail.com>
2020-12-07 05:50:55 +0100

    feat: edit template

--- template.py
+++ template.py
@@ -4,6 +4,8 @@ def solution(inp):
 if __name__ == "__main__":
     with open('x.in') as f:
         inp = f.readlines()
+    #with open('7.in') as f:
+        #inp = f.read().strip().split('\n\n')
     #inp = [l.strip() for l in inp]
     #inp = [int(l.strip()) for l in inp]
     #inp = [l.strip().split(':') for l in inp]
