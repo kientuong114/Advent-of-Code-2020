e5e440d626bf7a54465ccecae261b6b221676000
Kien Tuong Truong <kientuong@gmail.com>
2020-12-22 07:36:44 +0100

    refactor: small spacing changes in p1 of day 22

--- 22/1.py
+++ 22/1.py
@@ -4,13 +4,16 @@ def solution(inp):
     p1, p2 = inp
     p1 = deque([int(l) for l in p1.splitlines()[1:]])
     p2 = deque([int(l) for l in p2.splitlines()[1:]])
+
     while len(p1) and len(p2):
         p1c, p2c = p1.popleft(), p2.popleft()
         if p1c > p2c:
             p1.extend([p1c, p2c])
         else:
             p2.extend([p2c, p1c])
+
     p = p1 if len(p1) else p2
+
     return sum((i+1)*x for i, x in enumerate(reversed(p)))
 
 if __name__ == "__main__":
